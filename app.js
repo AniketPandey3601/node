03f5c7d6ec73b47bb29931753495316be2adc217
unknown <you@example.com>
2024-04-14 13:39:52 +0530

    express

--- app.js
+++ app.js
@@ -1,57 +1,20 @@
 const http = require('http');
-const fs = require('fs');
-let temp;
+const express = require('express');
+const app = express();
+
+app.use((req,res,next)=>{
+    console.log("in the middleware");
+
+    next();
+})
 
 
-
-const server = http.createServer((req, res)=>{
-
-    const url = req.url;
-    const method = req.method;
-    if(url === '/'){
-
-        res.write('<html>');
-
-        res.write('<head><title>Enter Message</title><head>');
-        
-        res.write(`<body><h1>${temp}</h1><br><form action = "/message" method = "POST"><input type= "text" name = "message"><button type = "submit">Send</button></form></body>`);
-        
-        res.write('</html>');
-    
-        return res.end();
-    }
-
-    if(url === '/message' && method === 'POST'){
-        const body=[];
-        req.on('data', (chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        })
-        req.on('end',()=>{
-            const parsebody = Buffer.concat(body).toString();
-            const msg = parsebody.split('=')[1];
-            console.log(parsebody);
-            fs.writeFileSync('message.txt',msg);
-            temp = msg;
-            
-        })
-        
-        res.statusCode = 302;
-        res.setHeader('Location','/');
-        return res.end();
-
-    }
-
-    res.setHeader('Content-Type' , 'text/html');
-    res.write('<html>');
-
-    res.write('<head><title>Welcome to my first Noe.js Project</title><head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-
-    res.end();
-   
-});
+app.use((req,res,next)=>{
+    console.log("in the  another middleware");
+    res.send('<h1>Hellow from express</h1>');
+})
 
 
-server.listen(4000);
+app.listen(4000);
+
+ 
