dc245c98277a28059ddd02fe8e9c697fd97dcf09
unknown <you@example.com>
2024-04-15 07:18:55 +0530

    middlewares

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const http = require('http');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 
-app.use((req,res,next)=>{
-    console.log("in the middleware");
 
-    next();
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action = "/product" method = "POST"><input type= "text" name= "title"><button type = "submit">Add Product</button></form>')
 })
 
+app.use('/product', (req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/');
+})
 
-app.use((req,res,next)=>{
+app.use('/',(req,res,next)=>{
     console.log("in the  another middleware");
-    res.send('<h1>Hellow from express</h1>');
+    res.send('<h1>Hello yash  from express</h1>');
 })
 
 
