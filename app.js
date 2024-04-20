d32857b8cf5822215fa3a5cff5192afbc5c5cc2e
unknown <you@example.com>
2024-04-20 13:14:27 +0530

    database

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const express = require('express');
 const app = express();
 const errorcontroller = require('./controllers/error')
+const db = require('./util/database')
 
 const adminRoutes = require('./routes/admin.js');
 const shoproutes = require('./routes/shop');
@@ -15,10 +16,13 @@ app.use(express.static(path.join(__dirname+'/public')))
 // app.use(express.static('public'));
 
 
-
+db.execute('SELECT * FROM products')
+.then(result=>{console.log(result[0] , result[1])})
+.catch(err=>{console.log(err)})
 
 app.use('/admin',adminRoutes);
 app.use('/shop', shoproutes);
+
 
 app.use('/contactus',(req,res,next)=>{
 
